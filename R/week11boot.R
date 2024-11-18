371d4ccc4d6ed01e745f7ecdfb4a5a461bbff71b
mugengliu <mugeng.liu@mail.utoronto.ca>
2024-11-19 00:08:48 +0800

    Week11 Bootstrap Analysis

--- R/week11boot.R
+++ R/week11boot.R
@@ -1,28 +1,9 @@
 library(readr)
 library(dplyr)
+library(boot)
 
 data <- read_csv("data/analytical/finaldata.csv")
 
-data_2017 <- filter(data, year == "2017")
-
-# first quiz answer
-sum(is.na(data_2017$matmor))
-
-
-data_no <- filter(data_2017, !is.na(matmor))
-
-# second quiz answer
-sum(data_no$armconf1 == 1, na.rm = TRUE)
-sum(data_no$armconf1 == 0, na.rm = TRUE)
-
-
-data_conf <- filter(data_no, armconf1 == 1)
-
-# third quiz answer
-mat_med <- median(data_conf$matmor, na.rm = TRUE)
-mat_med
-
-library(boot)
 
 getmeddiff <- function(data, indices) {
   sample_data <- data[indices, ]
